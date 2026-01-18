ec12542d6c2fbdf36f33a7be9ce078ff723b9696
Ryan Johanningmeier <ryan@ryanjo.com>
2026-01-18 07:32:32 -0600

    Application loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,33 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
+#include <iostream>
+
 #include "Window.h"
+
+void handleEvent(SDL_Event& e) {
+	if (e.type == SDL_EVENT_MOUSE_MOTION) {
+		std::cout << "Mouse moved.\n";
+	}
+	else if (e.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
+		std::cout << "Mouse clicked.\n";
+	}
+	else if (e.type == SDL_EVENT_KEY_DOWN) {
+		std::cout << "Keyboard button pressed.\n";
+	}
+}
 
 int main(int, char**) {
 	SDL_Init(SDL_INIT_VIDEO);
 	Window window;
 
-	bool isRunning = true;
 	SDL_Event e;
+	bool isRunning{ true };
 
 	while (isRunning) {
 		while (SDL_PollEvent(&e)) {
+			handleEvent(e);
+
 			if (e.type == SDL_EVENT_QUIT) {
 				isRunning = false;
 			}

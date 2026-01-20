09a6487306f02bb299178bb080a616aacded88bc
Ryan Johanningmeier <ryan@ryanjo.com>
2026-01-20 06:08:06 -0600

    Green and Red ... squares

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,27 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-#include <iostream>
-
 #include "Window.h"
-#include "Rectangle.h"
+#include "UI.h"
 
 int main(int, char**) {
 	SDL_Init(SDL_INIT_VIDEO);
 	Window window;
-	Rectangle rect{ SDL_Rect{50, 50, 50, 50} };
+	UI UIManager;
 
 	SDL_Event e;
 	bool isRunning{ true };
 
 	while (isRunning) {
 		while (SDL_PollEvent(&e)) {
-			rect.handleEvent(e);
+			UIManager.handleEvent(e);
 			if (e.type == SDL_EVENT_QUIT) {
 				isRunning = false;
 			}
 		}
 
 		window.render();
-		rect.render(window.getSurface());
+		UIManager.render(window.getSurface());
 		window.update();
 	}
 
